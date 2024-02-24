606cc0dcd1a137a5c3cc001faa3a7f8399e814ca
klajdi.hasani <hasaniklajdi17@gmail.com>
2024-02-24 21:29:56 +0100

    Create project structure files

---
+++ src/app/invoices-components/invoices-components.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InvoicesComponentsRoutingModule } from './invoices-components-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InvoicesComponentsRoutingModule
+  ]
+})
+export class InvoicesComponentsModule { }
