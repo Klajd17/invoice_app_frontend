87e4ea96d8987b4ab6f0820ed159aa9a7cce96a6
klajdi.hasani <hasaniklajdi17@gmail.com>
2024-02-24 22:06:52 +0100

    Import all necessary angular material modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,93 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatTabsModule} from "@angular/material/tabs";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatChipsModule} from "@angular/material/chips";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatStepperModule} from "@angular/material/stepper";
+import {AccordionAnchorDirective, AccordionDirective, AccordionLinkDirective} from "./components/accordion";
+import {MenuItems} from "./components/menu-items";
 
-
+const MaterialModules = [
+  FormsModule,
+  ReactiveFormsModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatCardModule,
+  MatSidenavModule,
+  MatListModule,
+  MatDividerModule,
+  MatProgressSpinnerModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatTableModule,
+  MatSortModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatNativeDateModule,
+  MatDatepickerModule,
+  MatTabsModule,
+  MatIconModule,
+  MatSlideToggleModule,
+  MatRadioModule,
+  MatGridListModule,
+  MatCheckboxModule,
+  MatExpansionModule,
+  MatSliderModule,
+  MatChipsModule,
+  MatBadgeModule,
+  MatStepperModule
+];
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccordionAnchorDirective,
+    AccordionLinkDirective,
+    AccordionDirective
+  ],
   imports: [
     CommonModule,
+    MaterialModules,
     SharedRoutingModule
-  ]
+  ],
+  exports: [
+    MaterialModules,
+    AccordionAnchorDirective,
+    AccordionLinkDirective,
+    AccordionDirective
+  ],
+  providers: [ MenuItems ]
 })
 export class SharedModule { }
