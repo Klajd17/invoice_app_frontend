5b50ce2f5286446fa5be05d84e0a0465fde48e18
klajdi.hasani <hasaniklajdi17@gmail.com>
2024-02-24 21:47:32 +0100

    Import some modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CoreModule} from "./core/core.module";
+import {SharedModule} from "./shared/shared.module";
+import {CustomerModule} from "./invoices-components/customer/customer.module";
+import {InvoiceModule} from "./invoices-components/invoice/invoice.module";
+import {ItemModule} from "./invoices-components/item/item.module";
 
 
 
@@ -15,6 +20,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
+        CoreModule,
+        SharedModule,
+        CustomerModule,
+        InvoiceModule,
+        ItemModule
     ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { FullComponent } from './components/layouts/full/full.component';
+import { HeaderComponent } from './components/layouts/full/header/header.component';
+import { SidebarComponent } from './components/layouts/full/sidebar/sidebar.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    FullComponent,
+    HeaderComponent,
+    SidebarComponent,
+  ],
   imports: [
     CommonModule,
     CoreRoutingModule
